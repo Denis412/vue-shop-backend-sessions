e675753f21bfe808fd60f2fdfbe5ab987450d7e0
Denis <47417617+Denis412@users.noreply.github.com>
2023-12-06 22:31:14 +0300

    f

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -23,7 +23,7 @@ export class UserService {
     const createdUser = await this.repository.save({
       id: generateId(),
       ...input,
-      username: `user${generateId().slice(0, 5)}`,
+      username: input.username,
       password: hashPassword,
     });
 
